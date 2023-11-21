01eb6c332281409f38e18b55af5f3dfa3c00e8df
fay <wahomeimani@gmail.com>
2023-11-21 15:20:24 +0300

    complete

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,35 @@
 #!/usr/bin/env python3
 
+# Task 1: happy_new_year
 def happy_new_year():
-    # code goes here!
-    pass
+    count = 10
+    while count >= 1:
+        print(count)
+        count -= 1
+    print("Happy New Year!")
 
+# Task 2: square_integers
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_numbers = [num ** 2 for num in int_list]
+    return squared_numbers
 
+# Task 3: fizzbuzz
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+
+# Test cases
+happy_new_year()
+
+numbers_to_square = [1, 2, 3, 4, 5]
+squared_result = square_integers(numbers_to_square)
+print(squared_result)
+
+fizzbuzz()
